11342ec6ed5faf2927317e63d17d3f3c54a5656c
rashfael <rashfael@isobeef.org>
2019-08-20 20:30:59 +0200

    eject default theme

---
+++ site/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+.theme-container
+	.theme-default-content
+		h1 404
+		blockquote {{ getMsg() }}
+		router-link(to='/') Take me home.
+</template>
+
+<script>
+const msgs = [
+	`There's nothing here.`,
+	`How did we get here?`,
+	`That's a Four-Oh-Four.`,
+	`Looks like we've got some broken links.`
+]
+
+export default {
+	methods: {
+		getMsg () {
+			return msgs[Math.floor(Math.random() * msgs.length)]
+		}
+	}
+}
+</script>
